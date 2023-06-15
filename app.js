0423610930df8350dfc4af861cdcbb973fb32119
Vitaly <naturbeer@mail.ru>
2023-06-15 18:37:05 +0300

    create render list

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log(123)
+const App = {
+  data() {
+    return {
+      title: "ToDo",
+      input: {
+        value: "",
+        placeholder: "Введите значение...",
+      },
+      notes: ["тили-тили", "трали-вали"],
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.notes.push(this.input.value);
+      this.input.value = "";
+      console.log(this.input.value);
+    },
+  },
+};
+
+Vue.createApp(App).mount("#app");
